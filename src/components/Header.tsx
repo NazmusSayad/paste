5f2b92b7e8a209d9d465102a69752298f6dca72d
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-05-04 15:42:57 +0000

    M src/components/Header.module.css  M src/components/Header.tsx  M src/components/ShowPaste.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,16 @@ import Wrapper from '../layout/Wrapper'
 import SavePaste from '../components/SavePaste'
 import css from './Header.module.css'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import { inter } from '../fonts'
 
 export default function ({ setStatus }) {
   const router = useRouter()
+
+  function handleCopyClick() {
+    const id = router.query.slag
+    const url = location.origin + '/' + id
+    navigator.clipboard.writeText(url)
+  }
 
   return (
     <div className={$cn(css.Header, inter.className)}>
@@ -14,12 +19,20 @@ export default function ({ setStatus }) {
         <div className={css.header}>
           <div className={css.logo}>Clip</div>
 
-          <div className={css.button}>
-            {router.route === '/' ? (
-              <SavePaste setStatus={setStatus} />
-            ) : (
-              <button onClick={() => router.push('/')}>New</button>
+          <div className={css.controls}>
+            {router.route === '/[slag]' && (
+              <div className={css.button}>
+                <button onClick={handleCopyClick}>Copy Link</button>
+              </div>
             )}
+
+            <div className={css.button}>
+              {router.route === '/' ? (
+                <SavePaste setStatus={setStatus} />
+              ) : (
+                <button onClick={() => router.push('/')}>New</button>
+              )}
+            </div>
           </div>
         </div>
       </Wrapper>
--- src/components/ShowPaste.tsx
+++ src/components/ShowPaste.tsx
@@ -1,15 +1,22 @@
+import Head from 'next/head'
 import Wrapper from '../layout/Wrapper'
 import css from './ShowPaste.module.css'
 
 const ShowPaste = ({ paste }) => {
   return (
-    <div className={css.container}>
-      <Wrapper>
-        {paste?.text?.split('\n').map((part, i) => (
-          <p key={i}>{part}</p>
-        ))}
-      </Wrapper>
-    </div>
+    <>
+      <Head>
+        <title>Pastei - {paste._id}</title>
+      </Head>
+
+      <div className={css.container}>
+        <Wrapper>
+          {paste?.text?.split('\n').map((part, i) => (
+            <p key={i}>{part}</p>
+          ))}
+        </Wrapper>
+      </div>
+    </>
   )
 }
 
