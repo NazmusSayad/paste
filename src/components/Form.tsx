d460587da2c856de2f6cb2a135b3277d8092fbd0
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-05-07 22:39:24 +0600

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
 import css from './Form.module.css'
 import Wrapper from '../layout/Wrapper'
 
 const Home: NextPage = () => {
   return (
     <Wrapper className={css.wrapper}>
+      <Head>
+        <title>Pastei - Create Paste</title>
+      </Head>
+      
       <textarea className={css.textarea} id="paste-text" />
     </Wrapper>
   )
