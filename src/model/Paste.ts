4173f87798be63cd0a9142593eaecec587a59869
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-05-07 15:05:08 +0000

    M src/model/Paste.ts

--- src/model/Paste.ts
+++ src/model/Paste.ts
@@ -1,5 +1,8 @@
+import * as pg from 'pg'
 import { Sequelize, DataTypes } from 'sequelize'
-const sequelize = new Sequelize(process.env.DB as any)
+const sequelize = new Sequelize(process.env.DB as any, {
+  dialectModule: pg,
+})
 
 const schema = {
   _id: {
